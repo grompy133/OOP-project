c2774600eeeb77edfb3d134822af6ae88c41f47b
BorissZubkovs <bzubkov95@gmail.com>
2025-03-07 21:48:00 +0200

    +#6

--- Backend/src/login.py
+++ Backend/src/login.py
@@ -32,7 +32,7 @@ DB_PASSWORD = 'msu8nTwIkf6isAR5qBmp'
 DB_DSN = "v9n3ba1erzl8nuba_high"
 DB_WALLET_PASSWORD = "dR3kQd8utf5jLyqRyeFx"
 DB_WALLET_LOCATION = r"C:\\Users\\Boris\\Desktop\\Wallet_V9N3BA1ERZL8NUBA"
-
+#6
 
 # Function to get database connection
 def get_db_connection():
